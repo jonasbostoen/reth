6dfa7f5c282f937d9bfc5c0aaff5e03556359da1
Jonas Bostoen <jonas.bostoen@hotmail.com>
2023-01-29 13:57:49 +0100

    patch(net): increase receiving session buffer size

--- crates/net/network/src/session/config.rs
+++ crates/net/network/src/session/config.rs
@@ -29,12 +29,12 @@ impl Default for SessionsConfig {
         SessionsConfig {
             // This should be sufficient to slots for handling commands sent to the session task,
             // since the manager is the sender.
-            session_command_buffer: 1024,
+            session_command_buffer: 4096,
             // This should be greater since the manager is the receiver. The total size will be
             // `buffer + num sessions`. Each session can therefore fit at least 1 message in the
             // channel. The buffer size is additional capacity. The channel is always drained on
             // `poll`.
-            session_event_buffer: 8192,
+            session_event_buffer: 16384,
             limits: Default::default(),
             request_timeout: INITIAL_REQUEST_TIMEOUT,
         }
